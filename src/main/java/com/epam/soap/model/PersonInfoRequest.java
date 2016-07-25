0d3d0e8e2140c5bf48852a9d0242ab732ea51a0c
Andrei_Yakushin <Andrei_Yakushin@epam.com>
2016-07-25 09:43:25 +0400

    model classes

---
+++ src/main/java/com/epam/soap/model/PersonInfoRequest.java
@@ -0,0 +1,26 @@
+package com.epam.soap.model;
+
+/**
+ * @author Andrei_Yakushin
+ * @since 7/25/2016 9:37 AM
+ */
+public class PersonInfoRequest {
+    private String name;
+    private String surname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
---
+++ src/main/java/com/epam/soap/model/PersonInfoResponse.java
@@ -0,0 +1,35 @@
+package com.epam.soap.model;
+
+/**
+ * @author Andrei_Yakushin
+ * @since 7/25/2016 9:38 AM
+ */
+public class PersonInfoResponse {
+    private String name;
+    private String surname;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
